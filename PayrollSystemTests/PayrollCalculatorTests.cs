64b2412b15974ac6ad32c232adefcac8faa22894
TSHIRE <168409719+GLOSSYYY@users.noreply.github.com>
2026-05-29 19:06:07 +0200

    Added 'UIF' and 'Membership' TestMethod

--- PayrollSystemTests/PayrollCalculatorTests.cs
+++ PayrollSystemTests/PayrollCalculatorTests.cs
@@ -8,7 +8,7 @@ namespace PayrollSystemTests
     {
         private PayrollCalculator calculator;
 
-        public TestContext TestContext {get; set; }
+        public TestContext TestContext { get; set; }
 
         [TestInitialize]
         public void Setup()
@@ -20,18 +20,45 @@ namespace PayrollSystemTests
         [TestCategory("Gross Pay")]
         public void CalculateGrossPay_ShouldReturnCorrectValue()
         {
-            decimal expected = 19000m; 
+            decimal expected = 19000m;
             decimal actual = calculator.CalculateGrossPay(20);
 
             TestContext.WriteLine($"Expected: {expected}");
             TestContext.WriteLine($"Actual: {actual}");
 
-            Assert.AreEqual(expected, actual); 
+            Assert.AreEqual(expected, actual);
 
         }
 
         [TestMethod]
         [TestCategory("UIF")]
-        
+        public void ClaculatedUIF_ShouldReturnCorrectValue()
+        {
+            decimal grossPay = 19000m;
+
+            decimal expected = 190m;
+
+            decimal actual = calculator.CalculateUIF(grossPay);
+            TestContext.WriteLine($"Expected: {expected}");
+            TestContext.WriteLine($"Actual: {actual}");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("Membership")]
+        public void CalculateMembership_ShouldReturnCorrectValue()
+        {
+            decimal grossPay = 19000m;
+
+            decimal expected = 2470m;
+
+            decimal actual = calculator.CalculateMembership(grossPay);
+            TestContext.WriteLine($"Expected: {expected}");
+            TestContext.WriteLine($"Actual: {actual}");
+
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
